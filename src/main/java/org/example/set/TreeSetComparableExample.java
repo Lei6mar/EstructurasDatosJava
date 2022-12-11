b664ea2bb9d011789d1d443b4d50006b61e632e2
miledesmam <miledesmam@gmail.com>
2022-12-11 13:39:01 -0600

    treeset comparable y comparator

---
+++ src/main/java/org/example/set/TreeSetComparableExample.java
@@ -0,0 +1,21 @@
+package org.example.set;
+
+import org.example.model.Alumno;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TreeSetComparableExample {
+
+    public static void main(String[] args) {
+        Set<Alumno> sa = new TreeSet<>((a,b) -> b.getNombre().compareTo(a.getNombre()));
+        sa.add(new Alumno("Pato", 5));
+        sa.add(new Alumno("Cata", 6));
+        sa.add(new Alumno("Luis", 4));
+        sa.add(new Alumno("Pedro", 9));
+        sa.add(new Alumno("Andres", 10));
+        sa.add(new Alumno("Zeus", 2));
+
+        System.out.println("sa = " + sa);
+    }
+}
