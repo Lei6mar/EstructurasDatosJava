29eda167960a33145609fa284004302af2881aa2
miledesmam <miledesmam@gmail.com>
2022-12-11 00:31:29 -0600

    TreeSets

--- src/main/java/org/example/set/TreeSet.java
+++
@@ -1,2 +0,0 @@
-package org.example.set;public class TreeSet {
-}
---
+++ src/main/java/org/example/set/TreeSetExample.java
@@ -0,0 +1,10 @@
+package org.example.set;
+
+import java.util.Set;
+
+public class TreeSet {
+
+    public static void main(String[] args) {
+        Set<String> ts = new java.util.TreeSet<>()
+    }
+}
